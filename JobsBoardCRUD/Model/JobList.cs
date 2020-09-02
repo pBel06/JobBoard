0a5b1a035cfc700768cb0c8a3cc99ff7f1714b8d
pBel06 <patty_eb18@hotmail.com>
2020-09-02 11:34:36 -0600

    JobList model

---
+++ JobsBoardCRUD/Model/JobList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobsBoardCRUD.Model
+{
+    public class JobList
+    {
+        public int ID { get; set; }
+        public string Job { get; set; }
+        public string JobTitle { get; set; }
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CreatedAt { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ExpiresAt { get; set; }
+    }
+}
